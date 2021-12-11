aee4e7dc2403d27bcd1f7da7edf937c0329c7125
nogeek <2385656176@qq.com>
2021-12-12 00:08:56 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
  * @LastEditors: weicong
  * @Description:
  * @Date: 2021-12-11 18:58:28
- * @LastEditTime: 2021-12-11 23:50:29
+ * @LastEditTime: 2021-12-12 00:08:35
  * @FilePath: \vite-project\src\main.js
  */
 import { createApp } from 'vue';
 import App from './App.vue';
+
+
+
 import '@/style/index.scss';
 
 createApp(App).mount('#app');
