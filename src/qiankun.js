31511239f42a0df2f64df84ac79ddc22015dc854
nogeek <2385656176@qq.com>
2022-01-03 20:11:59 +0800

    feat(build): df
    df

--- src/qiankun.js
+++ src/qiankun.js
@@ -3,7 +3,7 @@
  * @LastEditors: weicong
  * @Description:
  * @Date: 2022-01-01 14:55:59
- * @LastEditTime: 2022-01-03 20:04:32
+ * @LastEditTime: 2022-01-03 20:10:43
  * @FilePath: \vite-project\src\qiankun.js
  */
 
